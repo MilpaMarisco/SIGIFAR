3b68bfbd64703701a31f526e2077766bd20c0215
Amilpa <amilpamariscurrenacristian@gmail.com>
2025-10-09 21:25:28 -0600

    Reorganizado las carpetas

--- src/main/java/com/sigifar/util/DBConnection.java
+++ src/main/java/com/sigifar/util/DBConnection.java
@@ -4,11 +4,41 @@
  */
 
 package com.sigifar.util;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author amilp
  */
 public class DBConnection {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/sigifar";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
+    private Connection connection;
+
+    public DBConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
