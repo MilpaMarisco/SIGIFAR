ca781c00b3d94e69a314d74eb83f86a4a90ad2ed
Amilpa <amilpamariscurrenacristian@gmail.com>
2025-10-09 22:08:24 -0600

    Crud entradas bean y dao

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.sigifar {
     requires javafx.fxml;
     requires transitive javafx.graphics;
     requires javafx.web;
+    requires java.sql;
 
     opens com.sigifar.app to javafx.fxml;
     exports com.sigifar.app;
